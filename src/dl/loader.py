62927a0fa3eafa43953fce9025690a46c5be1504
Biobot <biobot94@gmail.com>
2022-11-16 19:30:13 +0800

    pipeline
    Signed-off-by: Biobot <biobot94@gmail.com>

--- src/dl/loader.py
+++ src/dl/loader.py
@@ -1,8 +1,9 @@
-from torch.utils.data import dataset, random_split
+from torch.utils.data import dataset, Subset, random_split
 from torchvision.transforms import transforms
 from src.utils.data import getPatchPandas
 from PIL import Image
 import torch
+import random
 
 class PatchDataset(dataset.Dataset):
     def __init__(self):
@@ -28,6 +29,35 @@ class PatchDataset(dataset.Dataset):
 
     def __len__(self):
         return len(self.data)
+
+class PermutePatchDataset(dataset.Dataset):
+    def __init__(self, indexlist):
+        super(PermutePatchDataset, self).__init__()
+        self.data = getPatchPandas()
+        self.data = self.data.loc[self.data.index[indexlist]].reset_index(drop=True)
+        self.score_data = list(self.data["SCORE"])
+        self.age_data = list(self.data["AGE_AT_VISIT"] / 100)
+        self.sex_data = list(self.data["SEX"])
+        self.duration_data = list(self.data["DURATION"] / 100)
+        self.tiv_data = list(self.data["TIV"] / 1000)
+        self.img_path = list(self.data["PATCH_PATH"])
+        self.transform = transforms.Compose([
+            transforms.Resize([32, 32]),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+    def __getitem__(self, index):
+        score = self.score_data[index]
+        img = self.transform(Image.open(self.img_path[index]))
+        labels = torch.FloatTensor([self.age_data[index], self.sex_data[index], self.duration_data[index], self.tiv_data[index]])
+        return img, labels, score
+
+    def __len__(self):
+        return len(self.data)
+    
+    def permute(self):
+        random.shuffle(self.score_data)
 
 def splitDataset(fold_size, fold_num, dataset):
     test_size = len(dataset) - fold_num * fold_size
